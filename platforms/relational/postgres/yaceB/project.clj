749c2d98d72e957a00b8eb4c1441d472eeeda1e3
Thomas Down <thomas.a.down@gmail.com>
2014-10-01 17:15:22 +0100

    Minimal web server for postgres.

---
+++ platforms/relational/postgres/yaceB/project.clj
@@ -0,0 +1,11 @@
+(defproject yaceB "0.0.1-SNAPSHOT"
+  :description "ACeDB/relational playgroup"
+  :dependencies [[org.clojure/tools.reader "0.8.3"]
+                 [org.clojure/clojure "1.6.0"]
+                 [hiccup "1.0.5"]
+                 [ring "1.3.1"]
+                 [compojure "1.1.6"]
+                 [clj-http "0.9.2"]
+                 [org.clojure/java.jdbc "0.3.5"]
+                 [com.mchange/c3p0 "0.9.2.1"]
+                 [postgresql "9.3-1102.jdbc41"]])
