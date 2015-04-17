8c6b0e79fcfccf8d474fc4c382bf56ac8d5dbb88
Michael Paulini <michael.paulini@wormbase.org>
2015-04-17 12:16:23 +0100

    some more changes
    more things to annoy @dasmoth :
    * reordered the if/else bit ... would have been nice to use a switch instead
    * added some rdoc comments in

--- pseudoace/ruby/server.rb
+++ pseudoace/ruby/server.rb
@@ -8,6 +8,7 @@ set :server, 'webrick'
 $conn = Diametric::Persistence::Peer.connect('datomic:free://localhost:4334/wb248-imp1')
 $db = $conn.db.to_java
 
+# convert gene datomic data into an anonymous hash
 def pack_gene(g)
   {:id       => g.get(':gene/id'),
    :label    => g.get(':gene/public-name'),
@@ -15,6 +16,7 @@ def pack_gene(g)
    :taxonomy => 'c_elegans'}
 end
 
+# convert person datomic data into an anonymous hash
 def pack_person(p)
   {:id       => p.get(':person/id'),
    :label    => p.get(':person/standard-name'),
@@ -22,8 +24,9 @@ def pack_person(p)
    :taxonomy => 'all'}
 end
 
+# sinatra url match / get
 get '/rest/widget/gene/:gid/history' do
-  gene = $db.entity([':gene/id', params['gid']])
+  gene = $db.entity([':gene/id', params['gid']]) #grab the gene based on the geneid
   json(
     {:name   => params['gid'],
      :class  => 'gene',
@@ -34,13 +37,17 @@ get '/rest/widget/gene/:gid/history' do
           :description => "The name and WormBase internal ID of #{gene.get(':gene/id')}"
        },
        :history => {
-         :data  => gene.get(':gene/version-change').map { |h| 
+          :data  => gene.get(':gene/version-change').map { |h| 
              r = {:version => h.get(':gene.version-change/version'),
                   :date    => h.get(':gene.version-change/date').toString,
                   :curator => pack_person(h.get(':gene.version-change/person')),
                   :remark  => nil,
                   :gene    => nil,
                   :action  => 'Unknown'}
+
+             merged   = h.get(':gene-history-action/merged-into')
+             acquires = h.get(':gene-history-action/acquires-merge')
+             imported = h.get(':gene-history-action/imported')
 
              if h.get(':gene-history-action/created')
                r[:action] = 'Created'
@@ -50,38 +57,25 @@ get '/rest/widget/gene/:gid/history' do
                r[:action] = 'Suppressed'
              elsif h.get(':gene-history-action/resurrected')
                r[:action] = 'Created'
-             end
-
-             merged = h.get(':gene-history-action/merged-into')
-             if merged
-               r[:action] = 'Merged_into'
-               r[:gene] = pack_gene(merged)
-             end
-
-             acquires = h.get(':gene-history-action/acquires-merge')
-             if acquires
-               r[:action] = 'Acquires_merge'
-               r[:gene] = pack_gene(acquires)
-             end
-
-             imported = h.get(':gene-history-action/imported')
-             if imported
-                 r[:action] = 'Imported'
-                 r[:remark] = imported.iterator.next;
-             end
-
-             if h.get(':gene-history-action/changed-class')
+             elsif h.get(':gene-history-action/transposon-in-origin')
+               r[:action] = 'Transposon_in_origin'
+             elsif h.get(':gene-history-action/changed-class')
                r[:action] = 'Changed_class'
                # What to do with old/new class?
-             end
-
-             if h.get(':gene-history-action/transposon-in-origin')
-               r[:action] = 'Transposon_in_origin'
+             elsif merged
+               r[:action] = 'Merged_into'
+               r[:gene] = pack_gene(merged)
+             elsif acquires
+               r[:action] = 'Acquires_merge'
+               r[:gene] = pack_gene(acquires)
+             elsif imported
+               r[:action] = 'Imported'
+               r[:remark] = imported.iterator.next;
              end
                
-             r
-         },
-         :description => 'the historical annotations of this gene'
-       }
+             r # return itself, as the object is not in the last line of the block
+          },
+          :description => 'the historical annotations of this gene'
+        }
     }})
 end
