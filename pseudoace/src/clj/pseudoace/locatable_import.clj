1fa2f71c6bddc8bb81289d77c9dcc4e2efd4b22f
Thomas Down <thomas.a.down@gmail.com>
2015-04-20 12:17:25 +0100

    Phase 1 homology importer.

--- pseudoace/src/clj/pseudoace/locatable_import.clj
+++ pseudoace/src/clj/pseudoace/locatable_import.clj
@@ -107,6 +107,59 @@
      {}
      splice-lines)))
 
+;;
+;; For version 1, we're *not* going to try collapsing Homol lines into single gapped alignments,
+;; since it seems that the blastp importer is only recording one line per sub-hit...
+;;
+;; this probably needs revisiting if we use this code for things other than ?Protein homols
+;;
+
+(defn- log-homols [homol-lines parent]
+  (reduce
+   (fn [log [[type target method score-s parent-start-s parent-end-s target-start-s target-end-s :as line] lines]]
+     (let [tid   [:importer/temp (d/squuid)]
+           start (parse-int parent-start-s)
+           end   (parse-int parent-end-s)
+           min   (if start (dec start))
+           max   end
+           bin   (if (and min max) (reg2bin min max))]
+       (update log (first (drop 3 (:timestamps (meta line))))
+         (fn [old]
+           (into (or old [])
+            (let [base (those
+                         [:db/add tid :locatable/parent parent]
+                         (and min [:db/add tid :locatable/min min])
+                         (and max [:db/add tid :locatable/max max])
+                         ;; ignore strand while we concentrate on protein stuff...
+                         #_[:db/add tid :locatable/strand (if (> start end)
+                                                            :locatable.strand/negative
+                                                            :locatable.strand/positive)]
+                         (and bin [:db/add tid :locatable/bin bin])
+                         (and bin [:db/add tid :locatable/xbin (xbin parent bin)])
+                         (and target-start-s [:db/add tid :homology/min (dec (parse-int target-start-s))])
+                         (and target-end-s [:db/add tid :homology/max      (parse-int target-end-s)   ]))]
+               (case type
+                 "DNA_homol"
+                 (except "Don't support protein->DNA homols")
+                 
+                 "Pep_homol"
+                 (conj base [:db/add tid :homology/protein [:protein/id target]])
+                 
+                 "Structure_homol"
+                 (conj base [:db/add tid :homology/structure [:structure-data/id target]])
+
+                 "Motif_homol"
+                 (conj base [:db/add tid :homology/motif [:motif/id target]])
+
+                 "Homol_homol"   ;; silently ignore
+                 nil
+
+                 ;; default
+                 (except "Don't understand homology type " type))))))))
+   {}
+   (group-by (partial take-ts 8) homol-lines)))
+              
+
 (defmulti log-locatables (fn [_ obj] (:class obj)))
 
 (defmethod log-locatables "Feature_data" [db obj]
@@ -120,6 +173,11 @@
        ;; Predicted_5 and Predicted_3 don't seem to be used
        ))
     (println "Couldn't find ?Feature_data " (:id obj))))
+
+(defmethod log-locatables "Protein" [db obj]
+  (if-let [protein (entity db [:protein/id (:id obj)])]
+    (log-homols (select-ts obj ["Homol"]) (:db/id protein))))
+                
 
 (defmethod log-locatables :default [_ _]
   nil)
