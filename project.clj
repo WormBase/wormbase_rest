40af7a8aea3b56f7bd5e3943bfb3c75fa8a38a98
Adam Wright <adam.wright@wormbase.org>
2016-04-06 18:27:43 +0000

    moving files around and removing dependencies on the project.clj

--- project.clj
+++ project.clj
@@ -5,9 +5,6 @@
                  [com.amazonaws/aws-java-sdk-dynamodb "1.9.39" :exclusions [joda-time]] ;; added
                  [datomic-schema "1.3.0"]
                  [wormbase/pseudoace "0.3.0"]
-                 [org.clojure/tools.cli "0.3.1"]
-                 [acetyl "0.0.9-SNAPSHOT"]
-                 [com.novemberain/monger "2.0.0"]
                  [hiccup "1.0.5"]
                  [ring "1.4.0"]
                  [fogus/ring-edn "0.2.0"]
@@ -23,38 +20,16 @@
                  [base64-clj "0.1.1"]
                  [cheshire "5.4.0"]
                  [ring/ring-anti-forgery "1.0.0"]
-                 [clj-time "0.9.0"]
-                 [org.clojure/data.csv "0.1.3"]
-
-                 [org.clojure/clojurescript "0.0-3308"]
-                 [org.omcljs/om "0.8.8"]
                  [secretary "1.2.3"]
-                 [com.andrewmcveigh/cljs-time "0.3.0"]
-                 
-                 [environ "1.0.0"]
+                 [environ "1.0.0"]]
 
-                 [org.clojure/tools.cli "0.3.3"]
-                 [org.clojars.hozumi/clj-commons-exec "1.0.6" ]]
   :description "REST API for getting data from datomic on a per widget basis"
-  :source-paths ["src/clj/"]
-  :java-source-paths ["src/java"]
-  :resource-paths ["resources"]
+  :source-paths ["src"]
 
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-environ "1.0.0"]]
+  :plugins [[lein-environ "1.0.0"]]
 
-  :cljsbuild {
-    :builds [{:id "dev"
-              :source-paths ["src/cljs"]
-              :compiler {
-                :optimizations :whitespace
-                :output-to "resources/public/js/main.js"
-                :output-dir "resources/public/js/out"
-                :source-map "resources/public/js/main.js.map"}}]}
-  
-  :javac-options ["-target" "1.6" "-source" "1.6"]
+  :javac-options ["-target" "1.8" "-source" "1.8"]
   :license "MIT"
-  :repositories [["dasmoth" {:url "http://www.biodalliance.org/people/thomas/repo"}]]
   :jvm-opts ["-Xmx6G"
              "-XX:+UseG1GC" "-XX:MaxGCPauseMillis=50"  ;; same GC options as the transactor,
                                                        ;; should minimize long pauses.
