62710f8d20c51ea26110a95142b65974d1725bb6
Matt Russell <matt@mgrbyte.co.uk>
2016-09-14 12:09:00 +0100

    Extract usage of env into a function.
    Do not use the `env` function in a global scope, otherwise environment
    variables will be read at *compliation* time instead of at runtime.

--- src/datomic_rest_api/utils/db.clj
+++ src/datomic_rest_api/utils/db.clj
@@ -3,13 +3,13 @@
             [environ.core :refer (env)]
             [datomic.api :as d]))
 
-(def uri (env :trace-db))
-
 (defn- new-datomic-connection [uri]
     (d/connect uri))
 
 (defn- datomic-disconnect [conn]
    (d/release conn))
 
-(defstate datomic-conn :start (new-datomic-connection uri)
-                       :stop (datomic-disconnect datomic-conn))
+(defstate datomic-conn
+  :start (fn []
+           (new-datomic-connection (env :trace-db))
+           :stop (datomic-disconnect datomic-conn))
