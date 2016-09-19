d62ec51c294f052c8853ca1380b49b03c2ae1fcd
Adam Wright <adam.wright@wormbase.org>
2016-09-19 19:13:13 +0000

    made it so that the dockerfile is minimal and is being created correctly

--- scripts/build-appjar.sh
+++ scripts/build-appjar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-artefact="target/app.jar"
+artefact="docker/app.jar"
 path=`lein with-profile +datomic-pro,+ddb ring uberjar | \
 	    sed -n 's|^Created \(\/.*standalone.jar\)|\1|p'`
 
