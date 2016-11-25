4e788f78f461d115cb5b76e13827710c82cbff56
Adam <adam.j.wright82@gmail.com>
2016-11-25 16:25:38 -0500

    change pseudoace version - no risks

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies
   [[org.clojure/clojure "1.8.0"]
    [datomic-schema "1.3.0"]
-   [wormbase/pseudoace "0.4.10"]
+   [wormbase/pseudoace "0.4.14"]
    [mount "0.1.10"]
    [hiccup "1.0.5"]
    [ring "1.5.0"]
