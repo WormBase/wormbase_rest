db10444967622d303ec2b67684a5d9700d64795b
Adam Wright <adam.wright@wormbase.org>
2016-12-12 20:26:13 +0000

    changing release version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wormbase/datomic-rest-api "0.0.7"
+(defproject wormbase/datomic-rest-api "0.0.8"
   :description "REST API for retrieving data from datomic on a per widget basis"
   :url "https://github.com/WormBase/datomic-to-catalyst"
   :min-lein-version "2.0.0"
