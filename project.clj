fb71a2de51ba62cbf03ce365e8c00015cd245a3d
Matt Russell <matt@mgrbyte.co.uk>
2017-01-31 20:46:54 +0000

    Updated change-log.
     * Bump version and mark as development version SNAPSHOT.
     * Added missing log for previous 0.1.0 release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wormbase/rest-api "0.1.0"
+(defproject wormbase/rest-api "0.1.1-SNAPSHOT"
   :description
   "REST API for retrieving data from datomic on a per widget basis"
   :url "https://github.com/WormBase/datomic-to-catalyst"
