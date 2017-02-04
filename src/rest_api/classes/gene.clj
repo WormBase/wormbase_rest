360ed3117b6e4cc7682812964c49a81f0ba386dc
Matt Russell <matt@mgrbyte.co.uk>
2017-02-04 14:54:04 +0000

    Fixed mis-named endpoint (phentotype not phenotypes)
    Renamed module to reflect this.

--- src/rest_api/classes/gene.clj
+++ src/rest_api/classes/gene.clj
@@ -7,12 +7,12 @@
    [rest-api.classes.gene.widgets.mapping-data :as mapping-data]
    [rest-api.classes.gene.widgets.ontology :as gene-ontology]
    [rest-api.classes.gene.widgets.overview :as overview]
-   [rest-api.classes.gene.widgets.phenotypes :as phenotypes]
+   [rest-api.classes.gene.widgets.phenotype :as phenotype]
    [rest-api.classes.gene.variation :as variation]
    [rest-api.routing :as routing]))
 
 (routing/defroutes
-  {:datatype "gene"
+  {:entity-class "gene"
    :widget
    {:external_links external-links/widget
     :feature feature/widget
@@ -21,7 +21,7 @@
     :history history/widget
     :mapping_data mapping-data/widget
     :overview overview/widget
-    :phenotypes phenotypes/widget}
+    :phenotype phenotype/widget}
    :field
    {:alles_other variation/alleles-other
     :polymorphisms variation/polymorphisms}})
