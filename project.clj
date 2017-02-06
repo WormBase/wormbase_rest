31e810c136657216384091376b039f73c3919cfe
Matt Russell <matt@mgrbyte.co.uk>
2017-02-06 03:30:45 +0000

    Ensures swagger.json produced by the application is valid.
    Fixes #59
    
     * validatorUrl re-enabled - swagger badge will now be shown.
       README with instructions on how to use in development.
       Production will use the default online swagger.io validator.
     * Disabled coercion in swagger api.
     * Explicitly state what the API `consumes` and `produces`.
     * Update tests accordingly
       Notably, the handler returned by make-request-handler no longer
       encodes responses to JSON itself. This is handled automatically
       by compojore.api via `ring-middleware-format`.

--- project.clj
+++ project.clj
@@ -37,29 +37,33 @@
              "-XX:+UseG1GC" "-XX:MaxGCPauseMillis=50"
              "-Ddatomic.objectCacheMax=2500000000"
              "-Ddatomic.txTimeoutMsec=1000000"]
-  :profiles {:datomic-free
-             {:dependencies [[com.datomic/datomic-free "0.9.5554"
-                              :exclusions [joda-time]]]}
-             :datomic-pro
-             {:dependencies [[com.datomic/datomic-pro "0.9.5554"
-                              :exclusions [joda-time]]]}
-             :ddb
-             {:dependencies
-              [[com.amazonaws/aws-java-sdk-dynamodb "1.11.6"
-                :exclusions [joda-time]]]}
-             :dev [:datomic-pro
-                   :ddb
-                   {:dependencies [[ring/ring-devel "1.5.1"]]
-                     :source-paths ["dev"]
-                     :env
-                     {:trace-db "datomic:ddb://us-east-1/WS257/wormbase"}
-                     :plugins [[jonase/eastwood "0.2.3"]
-                               [lein-ancient "0.6.8"]
-                               [lein-bikeshed "0.3.0"]
-                               [lein-ns-dep-graph "0.1.0-SNAPSHOT"]
-                               [venantius/yagni "0.1.4"]
-                               [com.jakemccrary/lein-test-refresh "0.17.0"]]
-                     :eastwood {:add-linters [:unused-namespaces]
-                                :exclude-namespaces [user]}
-                     :ring {:nrepl {:start? true}}}]}
+  :profiles
+  {:datomic-free
+   {:dependencies [[com.datomic/datomic-free "0.9.5554"
+                    :exclusions [joda-time]]]}
+   :datomic-pro
+   {:dependencies [[com.datomic/datomic-pro "0.9.5554"
+                    :exclusions [joda-time]]]}
+   :ddb
+   {:dependencies
+    [[com.amazonaws/aws-java-sdk-dynamodb "1.11.6"
+      :exclusions [joda-time]]]}
+   :dev [:datomic-pro
+         :ddb
+         {:dependencies [[ring/ring-devel "1.5.1"]]
+          :source-paths ["dev"]
+          :env
+          {:trace-db "datomic:ddb://us-east-1/WS257/wormbase"
+           :swagger-validator-url "http://localhost:8002"}
+          :plugins
+          [[jonase/eastwood "0.2.3"
+            :exclusions [org.clojure/clojure]]
+           [lein-ancient "0.6.8"]
+           [lein-bikeshed "0.3.0"]
+           [lein-ns-dep-graph "0.1.0-SNAPSHOT"]
+           [venantius/yagni "0.1.4"]
+           [com.jakemccrary/lein-test-refresh "0.17.0"]]
+          :eastwood {:add-linters [:unused-namespaces]
+                     :exclude-namespaces [user]}
+          :ring {:nrepl {:start? true}}}]}
   :repl-options {:init (set! *print-length* 10)})
--- src/rest_api/main.clj
+++ src/rest_api/main.clj
@@ -6,29 +6,54 @@
    [rest-api.classes.gene :as gene]
    [rest-api.classes.transcript :as transcript]))
 
-(defn init []
-  (mount/start))
-
 (def ^:private all-routes
+  "A collection of all routes to served by the application."
   [gene/routes
    transcript/routes])
 
-;; `validatorUrl` doesn't work with private urls, see:
-;; https://github.com/Orange-OpenSource/angular-swagger-ui/issues/43
+(def ^:private swagger-validator-url
+  "The URL used to validate the swagger JSON produced by the application."
+  (if-let [validator-url (environ/env :swagger-validator-url)]
+    validator-url
+    "//online.swagger.io"))
+
+(def ^:private api-output-formats
+  "The formats API endpoints will produce data in."
+  ["application/json"])
+
+(defn init
+  "Entry-point for ring server initialization."
+  []
+  (mount/start))
+
 (def app
+  "Entry-point for ring request handler."
   (sweet/api
    {:swagger
     {:ui "/"
      :spec "/swagger.json"
-     :options {:ui {:validatorUrl nil}}
+     :formats {:format api-output-formats}
+     :coercion nil
+     :consumes nil
+     :produces api-output-formats
+     :definitions {}
+     :options
+     {:ui
+      {:validatorUrl swagger-validator-url}}
      :data
      {:info
       {:title "WormBase REST API"
-       :description (str "Widget and field endpoints "
-                         "used by the official [WormBase]"
-                         "(http://www.wormbase.org) site.")}}}}
-   (sweet/context "/rest" []
-     :tags ["rest api"]
-     (->> all-routes
-          (flatten)
-          (apply sweet/routes)))))
+       :description
+       (str "Widget and field endpoints "
+            "used by the official [WormBase]"
+            "(http://www.wormbase.org) site.")
+       :contact {:name "WormBase development team"
+                 :email "developers@wormbase.org"}
+       :version (System/getProperty "rest-api.version")}}}}
+   (sweet/context "/" []
+     :middleware [ring-gzip/wrap-gzip]
+     (sweet/context "/rest" []
+       (->> all-routes
+            (flatten)
+            (apply sweet/routes))))))
+
