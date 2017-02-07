209b7452b78d6e3572ec41bde393df6cb9e3f54a
Juancarlos Chan <azurebrd@its.caltech.edu>
2017-02-07 02:41:08 +0000

    Working Publications widgets.

--- src/rest_api/classes/person.clj
+++ src/rest_api/classes/person.clj
@@ -1,21 +1,19 @@
 (ns rest-api.classes.person
   (:require
 ;;   [rest-api.classes.person.widgets.external-links :as external-links]
-   [rest-api.classes.person.widgets.overview :as overview]
-   [rest-api.classes.person.widgets.tracking :as tracking]
    [rest-api.classes.person.widgets.laboratory :as laboratory]
+   [rest-api.classes.person.widgets.overview :as overview]
+   [rest-api.classes.person.widgets.publications :as publications]
+   [rest-api.classes.person.widgets.tracking :as tracking]
    [rest-api.routing :as routing]))
 
 (routing/defroutes
   {:entity-class "person"
    :widget
    {
-    :overview overview/widget
-    :tracking tracking/widget
     :laboratory laboratory/widget
+    :overview overview/widget
+    :publications publications/widget
+    :tracking tracking/widget
    }})
 
-
-
-
-  
---
+++ src/rest_api/classes/person/widgets/publications.clj
@@ -0,0 +1,75 @@
+(ns rest-api.classes.person.widgets.publications
+  (:require
+   [clojure.string :as str]
+   [datomic.api :as d]
+   [pseudoace.utils :as pace-utils]
+   [rest-api.formatters.date :as date]
+   [rest-api.formatters.object :as obj :refer [pack-obj]]))
+
+
+(defn publications [person]
+         (def keymap)
+   (let [db (d/entity-db person)
+         dataArrayTuples
+         (->> (d/q '[:find [?paper ...]
+                   :in $ ?person
+                   :where [?paperperson :paper.person/person ?person]
+                          [?paper :paper/person ?paperperson]
+                          [?paper :paper/type ?typeent]
+                          (not [?typeent :paper.type/type :paper.type.type/meeting-abstract])
+                          ]
+                 db (:db/id person))
+              (map (fn [oid]
+                     (let [paper (d/entity db oid)]
+                      (pace-utils/vmap
+                       :year (first (str/split (:paper/publication-date paper) #"-"))
+                       :object
+                        (pace-utils/vmap
+                         :taxonomy "all"
+                         :class "paper"
+                         :label (:paper/brief-citation paper)
+                         :id (:paper/id paper))
+                       :brief_citation (:paper/brief-citation paper)
+                     ))))
+              (seq))]
+     (def data (group-by :year dataArrayTuples))
+     {:data (if (empty? data) nil data)
+   :description
+   "publications by this person excluding meeting abstracts."}))
+
+(defn meeting-abstracts [person]
+         (def keymap)
+   (let [db (d/entity-db person)
+         dataArrayTuples
+         (->> (d/q '[:find [?paper ...]
+                   :in $ ?person
+                   :where [?paperperson :paper.person/person ?person]
+                          [?paper :paper/person ?paperperson]
+                          [?paper :paper/type ?typeent]
+                          [?typeent :paper.type/type :paper.type.type/meeting-abstract]
+                          ]
+                 db (:db/id person))
+              (map (fn [oid]
+                     (let [paper (d/entity db oid)]
+                      (pace-utils/vmap
+                       :year (first (str/split (:paper/publication-date paper) #"-"))
+                       :object
+                        (pace-utils/vmap
+                         :taxonomy "all"
+                         :class "paper"
+                         :label (:paper/brief-citation paper)
+                         :id (:paper/id paper))
+                       :brief_citation (:paper/brief-citation paper)
+                     ))))
+              (seq))]
+     (def data (group-by :year dataArrayTuples))
+     {:data (if (empty? data) nil data)
+   :description
+   "meeting abstract publications by this person."}))
+
+
+(def widget
+  { :publications             publications
+    :meeting_abstracts        meeting-abstracts
+  })
+
