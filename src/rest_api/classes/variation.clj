7138aec4f407e8f4e29711950f31ef4f39414a83
Adam Wright <adam.wright@wormbase.org>
2017-02-20 20:37:24 +0000

    started isolation url

--- src/rest_api/classes/variation.clj
+++ src/rest_api/classes/variation.clj
@@ -2,6 +2,7 @@
   (:require
     [rest-api.classes.variation.widgets.overview :as overview]
     [rest-api.classes.variation.widgets.genetics :as genetics]
+    [rest-api.classes.variation.widgets.isolation :as isolation]
     [rest-api.classes.variation.widgets.external-links :as external-links]
     [rest-api.routing :as routing]))
 
@@ -10,4 +11,5 @@
    :widget
    {:overview overview/widget
     :genetics genetics/widget
+    :isolation isolation/widget
     :external-links external-links/widget}})
