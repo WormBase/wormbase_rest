f4e2fc1c70e49035ce35fad5f815ab9240e47789
Adam Wright <adam.wright@wormbase.org>
2017-05-25 15:41:28 +0000

    finished life_stage - can't realease because datomic is missing data

---
+++ src/rest_api/classes/life_stage/widgets/ontology_browser.clj
@@ -0,0 +1,6 @@
+(ns rest-api.classes.life-stage.widgets.ontology-browser
+  (:require
+   [rest-api.classes.generic :as generic]))
+
+(def widget
+  {:name generic/name-field})
--- src/rest_api/classes/life_stage/widgets/overview.clj
+++ src/rest_api/classes/life_stage/widgets/overview.clj
@@ -8,16 +8,20 @@
    [rest-api.formatters.object :as obj :refer [pack-obj]]))
 
 (defn preceded-by-life-stage [ls]
-  {:data (keys ls)
+  {:data (when-let [pbs (:life-stage/preceded-by ls)]
+           (for [pb pbs]
+             (pack-obj pb)))
    :dbid (:db/id ls)
    :description "preceded by life stage"})
 
 (defn cells [ls]
-  {:data nil
+  {:data (when-let [terms (:life-stage/anatomy-term ls)]
+           (for [term terms]
+             (pack-obj term)))
    :description "cells at this lifestage"})
 
 (defn cell-group [ls]
-  {:data nil
+  {:data nil ; this requires the cell group field and is commented out in the ace schema file. WBls:0000032 is an example of it populated.
    :description "The prominent cell group for this life stage"})
 
 (defn definition [ls]
@@ -27,25 +31,31 @@
 (defn contained-in-life-stage [ls]
   {:data (when-let [cis (:life-stage/contained-in ls)]
            (for [ci cis]
-             (pack-obj ci)
-             )
-           )
+             (pack-obj ci)))
    :description "contained in life stage"})
 
 (defn remarks [ls]
-  {:data nil
+  {:data (if-let [rs (:life-stage/remark ls)]
+           (for [r rs]
+             {:text (:life-stage.remark/text r)
+              :evidence nil}))
    :description "curatorial remarks for the Life_stage"})
 
 (defn substages [ls]
-  {:data nil
+  {:data (when-let [ss (:life-stage/_contained-in ls)]
+           (for [s ss]
+             (pack-obj s)))
    :description "life substage"})
 
 (defn followed-by-life-stage [ls]
-  {:data nil
+  {:data  (when-let [pbs (:life-stage/_preceded-by ls)]
+           (for [pb pbs]
+             (pack-obj pb)))
    :description "next life stage after this"})
 
 (defn other-names [ls]
-  {:data nil
+  {:data (when-let [ons (:life-stage/other-name ls)]
+           ons)
    :description (str "other names that have been used to refer to " (:life-stage/id ls))})
 
 (def widget
