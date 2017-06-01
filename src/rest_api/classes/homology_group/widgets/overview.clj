7a2be328ab7ec9e5775b74eebbee5928fb7c1c78
Adam Wright <adam.wright@wormbase.org>
2017-06-01 22:09:25 +0000

    finished homology - just missing one field. Might be fully done

--- src/rest_api/classes/homology_group/widgets/overview.clj
+++ src/rest_api/classes/homology_group/widgets/overview.clj
@@ -8,7 +8,12 @@
    [rest-api.formatters.object :as obj :refer [pack-obj]]))
 
 (defn proteins [h]
-  {:data nil
+  {:data (when-let [proteins (:homology-group/protein h)]
+           (for [protein proteins]
+             {:protein (pack-obj protein)
+              :species (when-let [species (:protein/species protein)]
+                         (pack-obj species))
+              :description (:protein/description protein)}))
    :description "proteins related to this homology_group"})
 
 (defn title [h]
@@ -16,11 +21,19 @@
    :description "title for this homology group"})
 
 (defn type-field [h]
-  {:data nil
+  {:data (not-empty
+           (cond
+             (contains? h :homology-group/cog-code)
+             {:homology_group "COG"
+              :evidence (obj/get-evidence (:homology-group/cog-code h))} ; not sure if this is how I should get evidence
+
+             (contains? h :homology-group/eggnog-code)
+             {:homology_group "eggNOG"
+              :evidence (obj/get-evidence (:homology-group/eggnog-code h))}))
    :description "type of homology group"})
 
-(defn gene-ontology-terms [g]
-  {:data nil
+(defn gene-ontology-terms [h]
+  {:data nil ; This requires  :homology-group/go-term  but there are no instances in the db of it existing
    :description "gene ontology terms associated to this homology group"})
 
 (def widget
