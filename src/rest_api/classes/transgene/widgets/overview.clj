0cd5631bec24330c5997d5c4dd2e3f108f8933a3
Adam Wright <adam.wright@wormbase.org>
2017-06-02 12:53:32 +0000

    finished most of transgene - just leaving used_for and reporter is not getting any hits when querying datomic - not sure if there should be some

--- src/rest_api/classes/transgene/widgets/overview.clj
+++ src/rest_api/classes/transgene/widgets/overview.clj
@@ -8,36 +8,98 @@
    [rest-api.formatters.object :as obj :refer [pack-obj]]))
 
 (defn other-reporter [t]
-  {:data nil
+  {:data (when-let [constructs (:construct/_transgene-construct t)]
+           (not-empty
+             (remove
+               nil?
+               (first ; flatten didn't seem to work here - should really use flatten: WBTransgene00000012
+                   (for [construct constructs]
+                     (:construct/other-reporter construct))))))
    :description "other reporters of this construct"})
 
 (defn utr [t]
-  {:data nil
+  {:data (when-let [constructs (:construct/_transgene-construct t)]
+           (not-empty
+             (remove
+               nil?
+               (flatten
+                 (for [construct constructs]
+                   (when-let [ghs (:construct/three-prime-utr construct)]
+                     (for [gh ghs]
+                       (pack-obj (:construct.three-prime-utr/gene gh)))))))))
    :description "3' UTR for this transgene"})
 
 (defn used-for [t]
-  {:data nil
+  {:data nil ; very similar to what is in construct class - WBTransgene00000001
    :description "The Transgene is used for"})
 
-(defn marked-rearrangement [t]
-  {:data nil
+(defn marked-rearrangement [t] ; also this one does not work in the Ace version
+  {:data (when-let [markers (:transgene/marker-for t)]
+           (:transgene.marker-for/text (first markers)))
    :description "rearrangements that the transgene can be used as a marker for"})
 
+(defn fusion-reporter [t]
+  {:data (when-let [constructs (:construct/_transgene-construct t)]
+           (not-empty
+             (remove
+               nil?
+               (flatten
+                 (for [construct constructs]
+                   (when-let [frs (:construct/fusion-reporter construct)]
+                     (for [fr frs]
+                       (obj/tag-obj fr))))))))
+   :description "reporter construct for this construct"})
+
 (defn synonym [t]
-  {:data nil
+  {:data (:transgene/synonym t)
    :description "a synonym for the transgene"})
 
-(defn purification-tag [t]
-  {:data nil
+(defn driven-by-gene [t]
+  {:data (when-let [constructs (:construct/_transgene-construct t)]
+           (not-empty
+             (remove
+               nil?
+               (flatten
+                 (for [construct constructs]
+                   (when-let [genes (:construct/driven-by-gene construct)]
+                     (for [gene genes]
+                       (pack-obj (:construct.driven-by-gene/gene gene)))))))))
+   :description "gene that drives the transgene"})
+
+(defn purification-tag [t] ; This one is not producing data from Ace
+  {:data (when-let [constructs (:construct/_transgene-construct t)]
+           (not-empty
+             (remove
+               nil?
+               (flatten
+                 (for [construct constructs]
+                   (when-let [tags (:construct/purification-tag construct)]
+                     (for [tag tags]
+                       (obj/tag-obj tag))))))))
    :description "the purification tag for the construct"})
 
 (defn strains [t]
-  {:data nil
+  {:data (when-let [strains (:transgene/strain t)]
+           (for [strain strains] (pack-obj strain)))
    :description "Strains associated with this transgene"})
 
 (defn recombination-site [s]
-  {:data nil
+  {:data nil ; no examples available in datomic based on query
+   :k (keys s)
+   :d (:db/id s)
    :description "map position of the integrated transgene"})
+
+(defn gene-product [t]
+  {:data  (when-let [constructs (:construct/_transgene-construct t)]
+           (not-empty
+             (remove
+               nil?
+               (flatten
+                 (for [construct constructs]
+                   (when-let [genes (:construct/gene construct)]
+                     (for [gene genes]
+                       (pack-obj (:construct.gene/gene gene)))))))))
+   :description "gene products for this transgene"})
 
 (def widget
   {:name generic/name-field
@@ -46,12 +108,12 @@
    :used_for used-for
    :marked_rearrangement marked-rearrangement
    :taxonomy generic/taxonomy
-   :fusion_reporter generic/fusion-reporter
+   :fusion_reporter fusion-reporter
    :summary generic/summary
    :synonym synonym
-   :driven_by_gene generic/driven-by-gene
+   :driven_by_gene driven-by-gene
    :purification_tag purification-tag
    :strains strains
    :remarks generic/remarks
    :recombination_site recombination-site
-   :gene_product generic/gene-product})
+   :gene_product gene-product})
