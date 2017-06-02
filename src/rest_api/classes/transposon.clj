2af05d6090150b6792856f1ae845d6230a318456
Adam Wright <adam.wright@wormbase.org>
2017-06-02 16:19:12 +0000

    shouldn't have commented out this one

--- src/rest_api/classes/transposon.clj
+++ src/rest_api/classes/transposon.clj
@@ -1,10 +1,9 @@
 (ns rest-api.classes.transposon
   (:require
-    ;[rest-api.classes.transposon.widgets.overview :as overview]
+    [rest-api.classes.transposon.widgets.overview :as overview]
     [rest-api.routing :as routing]))
 
 (routing/defroutes
   {:entity-ns "transposon"
    :widget
-   {;:overview overview/widget
-    }})
+   {:overview overview/widget}})
