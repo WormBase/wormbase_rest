57fc8f804195cb5f2ae0642d01e5cf9077ffb01d
Adam <adam.j.wright82@gmail.com>
2017-06-05 16:21:46 -0400

    changing to when-let from if-let

--- src/rest_api/classes/antibody/widgets/overview.clj
+++ src/rest_api/classes/antibody/widgets/overview.clj
@@ -5,29 +5,29 @@
    [rest-api.formatters.object :as obj :refer [pack-obj]]))
 
 (defn corresponding-gene [antibody]
-  {:data (if-let [holders (:antibody/gene antibody)]
+  {:data (when-let [holders (:antibody/gene antibody)]
            (pack-obj (:antibody.gene/gene (first holders))))
    :description "the corresponding gene the antibody was generated against"})
 
 (defn antigen [antibody]
-  {:data {:comment (if-let [antigen (:antibody/antigen antibody)]
+  {:data {:comment (when-let [antigen (:antibody/antigen antibody)]
                       (:antibody.antigen/text antigen))
-          :type (if-let [antigen (:antibody/antigen antibody)]
+          :type (when-let [antigen (:antibody/antigen antibody)]
                     (str/capitalize (name (:antibody.antigen/value antigen))))}
    :description "the type and decsription of antigen this antibody was generated against"})
 
 (defn constructed-by [antibody] ; no examples in datomic
-  {:data (if-let [person (:antibody/person antibody)]
+  {:data (when-let [person (:antibody/person antibody)]
               (pack-obj (first person)))
    :description "the person who constructed the antibody"})
 
 (defn clonality [antibody]
-  {:data (if-let [clonality-kw (:antibody/clonality antibody)]
+  {:data (when-let [clonality-kw (:antibody/clonality antibody)]
           (str/capitalize (name clonality-kw)))
    :description "the clonality of the antibody"})
 
 (defn animal [antibody]
-  {:data (if-let [animal-value (:antibody.animal/value (:antibody/animal antibody))]
+  {:data (when-let [animal-value (:antibody.animal/value (:antibody/animal antibody))]
             (str/capitalize (name animal-value)))
    :description "the animal the antibody was generated in"})
 
