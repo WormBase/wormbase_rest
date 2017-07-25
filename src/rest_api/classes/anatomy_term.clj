43b6e2c39307e4c00d13ccd90c3266d267e59145
Adam Wright <adam.wright@wormbase.org>
2017-07-25 20:58:02 +0000

    removing references link from anatomy-term page and removing entity mapping that did not make sense

--- src/rest_api/classes/anatomy_term.clj
+++ src/rest_api/classes/anatomy_term.clj
@@ -2,15 +2,12 @@
   (:require
     [rest-api.classes.gene.widgets.external-links :as external-links]
     ;[rest-api.classes.anatomy-term.widgets.overview :as overview]
-    [rest-api.classes.anatomy-term.widgets.references :as references]
     [rest-api.classes.anatomy-term.widgets.ontology-browser :as ontology-browser]
     [rest-api.routing :as routing]))
 
 (routing/defroutes
-  {:entity-ns "anatomy-function"
-   :uri-name "anatomy_term"
+  {:entity-ns "anatomy-term"
    :widget
    {;:overview overview/widget
     :ontology_browser ontology-browser/widget
-    :external_links external-links/widget
-    :references references/widget}})
+    :external_links external-links/widget}})
