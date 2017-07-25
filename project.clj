365801ffa92486eb067069d5c06768fc8a93b168
Sibyl <sibyl@wormbase.org>
2017-07-25 12:48:46 -0400

    remove snapshot from 0.1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wormbase/rest-api "0.1.8-SNAPSHOT"
+(defproject wormbase/rest-api "0.1.8"
   :description
   "REST API for retrieving data from datomic on a per widget basis"
   :url "https://github.com/WormBase/datomic-to-catalyst"
