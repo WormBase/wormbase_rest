ab6a0d68bb32e9d973c547c542d2ebab66410939
Adam Wright <adam.wright@wormbase.org>
2017-08-01 16:52:33 +0000

    commenting out clone widgets because it is missing data on some clones

--- src/rest_api/classes/clone.clj
+++ src/rest_api/classes/clone.clj
@@ -1,15 +1,16 @@
 (ns rest-api.classes.clone
   (:require
-    [rest-api.classes.gene.widgets.external-links :as external-links]
+    ;[rest-api.classes.gene.widgets.external-links :as external-links]
     ;[rest-api.classes.clone.widgets.overview :as overview]
-    [rest-api.classes.clone.widgets.location :as location]
-    [rest-api.classes.clone.widgets.references :as references]
+    ;[rest-api.classes.clone.widgets.location :as location]
+    ;[rest-api.classes.clone.widgets.references :as references]
     [rest-api.routing :as routing]))
 
 (routing/defroutes
   {:entity-ns "clone"
    :widget
    {;:overview overview/widget; needs access to field not available in datomic schema
-    :location location/widget
-    :external_links external-links/widget
-    :references references/widget}})
+    ;:location location/widget
+    ;:external_links external-links/widget
+    ;:references references/widget
+    }})
