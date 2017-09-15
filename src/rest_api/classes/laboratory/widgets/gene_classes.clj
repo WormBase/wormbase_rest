6c8500d29e7f8850d3096b3e83adc153d1579d02
Juancarlos Chan <azurebrd@its.caltech.edu>
2017-09-15 20:37:26 +0000

    Made some corrections based on Matt's suggestions.

--- src/rest_api/classes/laboratory/widgets/gene_classes.clj
+++ src/rest_api/classes/laboratory/widgets/gene_classes.clj
@@ -14,17 +14,14 @@
                          [?gc :gene-class/id ?gene-class]
                          [?gc :gene-class/description ?description]]
                        db (:db/id laboratory))
-                  (map (fn [oid]
-                         (let [gene-class (get oid 0)
-                               description (get oid 1)]
-                           (pace-utils/vmap
-                             :description description
-                             :former_gene_class
-                             (pace-utils/vmap
-                               :taxonomy "all"
-                               :class "gene_class"
-                               :label gene-class
-                               :id gene-class)))))
+                  (map (fn [result]
+                         (let [[gene-class description] result]
+                           {:description description
+                            :former_gene_class
+                            {:taxonomy "all"
+                             :class "gene_class"
+                             :label gene-class
+                             :id gene-class}})))
                   (seq))]
     {:data data
      :description "former gene classes assigned to laboratory"}))
@@ -38,17 +35,14 @@
                          [?gc :gene-class/id ?gene-class]
                          [?gc :gene-class/description ?description]]
                        db (:db/id laboratory))
-                  (map (fn [oid]
-                         (let [gene-class (get oid 0)
-                               description (get oid 1)]
-                           (pace-utils/vmap
-                             :description description
-                             :gene_class
-                             (pace-utils/vmap
-                               :taxonomy "all"
-                               :class "gene_class"
-                               :label gene-class
-                               :id gene-class)))))
+                  (map (fn [result]
+                         (let [[gene-class description] result]
+                           {:description description
+                            :gene_class
+                            {:taxonomy "all"
+                             :class "gene_class"
+                             :label gene-class
+                             :id gene-class}})))
                   (seq))]
     {:data data
      :description "gene classes assigned to laboratory"}))
