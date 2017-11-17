21110c406c1f51fefcd7d337f48b63ea8cc779bd
Adam Wright <adam.wright@wormbase.org>
2017-11-17 15:20:51 +0000

    adding wbprocess molecule widget

--- src/rest_api/classes/wbprocess.clj
+++ src/rest_api/classes/wbprocess.clj
@@ -2,6 +2,7 @@
   (:require
     [rest-api.classes.wbprocess.widgets.overview :as overview]
     [rest-api.classes.wbprocess.widgets.pathways :as pathways]
+    [rest-api.classes.wbprocess.widgets.molecule :as molecule]
     [rest-api.classes.wbprocess.widgets.phenotypes :as phenotypes]
     [rest-api.classes.wbprocess.widgets.references :as references]
     [rest-api.routing :as routing]))
@@ -12,4 +13,5 @@
    {:overview overview/widget
     :phenotypes phenotypes/widget
     :pathways pathways/widget
+    :molecule molecule/widget
     :references references/widget}})
