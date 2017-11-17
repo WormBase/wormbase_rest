0a3bdf962eaee3ac1784b2760e9bbebc5763c20e
Adam Wright <adam.wright@wormbase.org>
2017-11-17 19:36:23 +0000

    adding wbprocess go_term widget

--- src/rest_api/classes/wbprocess.clj
+++ src/rest_api/classes/wbprocess.clj
@@ -4,6 +4,7 @@
     [rest-api.classes.wbprocess.widgets.pathways :as pathways]
     [rest-api.classes.wbprocess.widgets.molecule :as molecule]
     [rest-api.classes.wbprocess.widgets.genes :as genes]
+    [rest-api.classes.wbprocess.widgets.go-term :as go-term]
     [rest-api.classes.wbprocess.widgets.phenotypes :as phenotypes]
     [rest-api.classes.wbprocess.widgets.references :as references]
     [rest-api.routing :as routing]))
@@ -15,5 +16,6 @@
     :phenotypes phenotypes/widget
     :pathways pathways/widget
     :molecule molecule/widget
+    :go_term go-term/widget
     :genes genes/widget
     :references references/widget}})
