7308daff240a2c1cda8379d447cac6e9579c6b73
Adam Wright <adam.wright@wormbase.org>
2017-11-21 14:58:33 +0000

    adding operon structure widget

--- src/rest_api/classes/operon.clj
+++ src/rest_api/classes/operon.clj
@@ -3,6 +3,7 @@
     [rest-api.classes.operon.widgets.overview :as overview]
     [rest-api.classes.operon.widgets.feature :as feature]
     [rest-api.classes.operon.widgets.location :as location]
+    [rest-api.classes.operon.widgets.structure :as structure]
     [rest-api.classes.operon.widgets.references :as references]
     [rest-api.routing :as routing]))
 
@@ -11,5 +12,6 @@
    :widget
    {:overview overview/widget
     :location location/widget
+    :structure structure/widget
     :feature feature/widget
     :references references/widget}})
