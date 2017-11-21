05c10cdfafd37dcfe1161f8b492d1fbca0a1bc58
Adam Wright <adam.wright@wormbase.org>
2017-11-21 22:58:10 +0000

    started reagents cds widget

---
+++ src/rest_api/classes/cds/widgets/reagents.clj
@@ -0,0 +1,35 @@
+(ns rest-api.classes.cds.widgets.reagents
+  (:require
+    [rest-api.classes.generic-fields :as generic]))
+
+(defn microarray-assays [c]
+  {:data nil
+   :description "The Microarray assays in this region of the sequence"})
+
+(defn orfeome-assays [c]
+  {:data nil
+   :description "The ORFeome Assays of the sequence"})
+
+(defn source-clone [c]
+  {:data (first
+           (some->> (:sequence/clone
+                      (:locatable/parent s))
+                    (map pack-obj)))
+   :description "The Source clone of the sequence"})
+
+(defn pcr-products [c]
+  {:data (some->> (:cds/corresponding-pcr-product s)
+                  (map pack-obj))
+   :description "PCR products for the sequence"})
+
+(defn matching-cdnas [c]
+  {:data nil
+   :description "cDNAs that match the sequence"})
+
+(def widget
+  {:name generic/name-field
+   :microarray_assays microarray-assays
+   :orfeome_assays orfeome-assays
+   :source_clone source-clone
+   :pcr_products pcr-products
+   :matching_cdnas matching-cdnas})
