fc5964fe3526ecff1ed32bd44391a0332984741d
Adam Wright <adam.wright@wormbase.org>
2017-11-21 16:12:58 +0000

    adding molecule strains widget

--- src/rest_api/classes/molecule.clj
+++ src/rest_api/classes/molecule.clj
@@ -3,6 +3,7 @@
     [rest-api.classes.gene.widgets.external-links :as external-links]
     ;[rest-api.classes.molecule.widgets.overview :as overview]
     [rest-api.classes.molecule.widgets.structure :as structure]
+    [rest-api.classes.molecule.widgets.strains :as strains]
     [rest-api.classes.molecule.widgets.references :as references]
     [rest-api.routing :as routing]))
 
@@ -11,5 +12,6 @@
    :widget
    {;:overview overview/widget
     :structure structure/widget
+    :strains strains/widget
     :external_links external-links/widget
     :references references/widget}})
