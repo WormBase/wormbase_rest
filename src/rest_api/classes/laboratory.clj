e97e175e8a37fc7087ed8ded0fbc0e204cc21f28
Juancarlos Chan <azurebrd@its.caltech.edu>
2017-12-11 03:00:41 +0000

    Lab Members widget for Laboratory class

--- src/rest_api/classes/laboratory.clj
+++ src/rest_api/classes/laboratory.clj
@@ -2,7 +2,7 @@
   (:require
     [rest-api.classes.laboratory.widgets.alleles :as alleles]
     [rest-api.classes.laboratory.widgets.gene-classes :as gene-classes]
-;;     [rest-api.classes.laboratory.widgets.members :as members]
+    [rest-api.classes.laboratory.widgets.members :as members]
     [rest-api.classes.laboratory.widgets.overview :as overview]
     [rest-api.classes.laboratory.widgets.strains :as strains]
     [rest-api.routing :as routing]))
@@ -12,6 +12,6 @@
    :widget
    {:alleles alleles/widget
     :gene_classes gene-classes/widget
-;;     :members members/widget
+    :members members/widget
     :overview overview/widget
     :strains strains/widget}})
