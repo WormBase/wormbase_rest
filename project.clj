127cf9c21b300adafbdd76baa8b53c59c24f4911
Adam Wright <adam.wright@wormbase.org>
2018-01-09 18:01:03 +0000

    prep for extending proxy timeout minor release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wormbase/rest-api "0.3.0"
+(defproject wormbase/rest-api "0.3.1"
   :description
   "REST API for retrieving data from datomic on a per widget basis"
   :url "https://github.com/WormBase/datomic-to-catalyst"
