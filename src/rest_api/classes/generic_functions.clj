701f7b2474a2d6bff15f7867d403b130be5efc66
Sibyl <sibyl@wormbase.org>
2018-02-08 22:44:16 +0000

    fix xform-species-name function to be null-safe WormBase/website#6160

--- src/rest_api/classes/generic_functions.clj
+++ src/rest_api/classes/generic_functions.clj
@@ -6,10 +6,11 @@
   "Transforms a `species-name` from the WB database into
   a name used to look up connection configuration to a sequence db."
   [species]
-  (let  [species-name-parts (str/split species #" ")
-         g  (str/lower-case  (ffirst species-name-parts))
-         species  (second species-name-parts)]
-    (str/join "_"  [g species])))
+  (if species
+    (let [species-name-parts (str/split species #" ")
+          g (str/lower-case (ffirst species-name-parts))
+          species (second species-name-parts)]
+      (str/join "_" [g species]))))
 
 (defn certainty [h]
   (cond
