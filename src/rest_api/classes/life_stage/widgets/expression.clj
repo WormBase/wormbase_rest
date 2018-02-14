d6125af54478705f75d25444d63f1db79ffafc58
Adam Wright <adam.wright@wormbase.org>
2018-02-14 23:40:56 +0000

    adding lifestage expression widget

---
+++ src/rest_api/classes/life_stage/widgets/expression.clj
@@ -0,0 +1,18 @@
+(ns rest-api.classes.life-stage.widgets.expression
+  (:require
+    [rest-api.formatters.object :as obj :refer [pack-obj]]
+    [rest-api.classes.expr-pattern.core :as expr-pattern]
+    [rest-api.classes.generic-functions :as generic-functions]
+    [rest-api.classes.generic-fields :as generic]))
+
+
+(defn expression-patterns [l]
+  {:data (some->> (:expr-pattern.life-stage/_life-stage l)
+                  (map (fn [h]
+                         (let [ep (:expr-pattern/_life-stage h)]
+                           (expr-pattern/pack ep)))))
+   :description (str "expression patterns associated with the Life_stage: " (:life-stage/id l))})
+
+(def widget
+  {:name generic/name-field
+   :expression_patterns expression-patterns})
