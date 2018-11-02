0b33e49c869e6bdb5951cc5deed51a4aa727ae65
Adam Wright <adam.wright@wormbase.org>
2018-11-02 16:37:04 +0000

    worked on completing this widget - ready for testing in WS268

--- src/rest_api/classes/feature/widgets/molecular_details.clj
+++ src/rest_api/classes/feature/widgets/molecular_details.clj
@@ -1,21 +1,85 @@
 (ns rest-api.classes.feature.widgets.molecular-details
   (:require
+    [clojure.string :as str]
+    [rest-api.classes.sequence.core :as sequence-fns]
     [rest-api.formatters.object :as obj :refer  [pack-obj]]
     [rest-api.classes.generic-fields :as generic]))
 
+(defn- reverse-complement [dna]
+  (str/replace
+    (str/reverse dna)
+    #"A|C|G|T|a|c|g|t"
+    {"A" "T"
+     "C" "G"
+     "G" "C"
+     "T" "A"
+     "a" "t"
+     "c" "g"
+     "g" "c"
+     "t" "a"
+     "-" "-"}))
+
 (defn flanking-sequences [f]
-  {:data {:flanks [(:feature.flanking-sequences/five-prime (:feature/flanking-sequences f))
-                   (:feature.flanking-sequences/three-prime (:feature/flanking-sequences f))]
-          :feture_seq nil
-          :seq nil 
-          :sequences  [{:sequence (:feature.flanking-sequences/five-prime (:feature/flanking-sequences f))
-                        :comment "flanking sequence (upstream)"}
-                       {:sequence (:feature.flanking-sequences/three-prime (:feature/flanking-sequences f))
-                        :comment "flanking sequence (downstream)"}]}
+  {:data (when-let [s (:locatable/parent f)]
+           (let [five-prime-flank (:feature.flanking-sequences/five-prime (:feature/flanking-sequences f))
+                 three-prime-flank (:feature.flanking-sequences/three-prime (:feature/flanking-sequences f))
+                 flanks [five-prime-flank three-prime-flank]
+                 seq-obj (pack-obj s)
+                 method (-> f :locatable/method :method/id)]
+             (if (or (some #(= method %) ["SL1" "SL2" "polyA_site" "history feature"])
+                     (not (contains? f :locatable/min)))
+               {:flanks flanks  ; section tested with WBsf000519
+                :seq seq-obj
+                :sequences [{:sequence five-prime-flank
+                             :comment "flanking sequence (upstream)"}
+                            {:sequence three-prime-flank
+                             :comment "flanking sequence (downstream)"}]})
+             (let [feature-start (:locatable/min f) ;tested with WBsf019129
+                   feature-end (:locatable/max f)
+                   padding 30
+                   refseqobj (sequence-fns/genomic-obj f)
+                   s (sequence-fns/get-sequence
+                       (conj
+                         refseqobj
+                         {:start (- (:start refseqobj) padding)
+                          :stop (+ padding (:stop refseqobj))}))
+                   padding-start (if (> (count five-prime-flank) padding)
+                                   (subs five-prime-flank padding)
+                                   five-prime-flank)
+                   padding-end (if (> (count three-prime-flank) padding)
+                                 (subs three-prime-flank
+                                       (- (count three-prime-flank) padding)
+                                       (count three-prime-flank))
+                                 three-prime-flank)
+                   strand (if (and (str/includes? s padding-start)
+                                   (str/includes? s padding-end))
+                            "positive" "negative")
+                   processed-sequence (if (= strand "negative")
+                                        (reverse-complement s)
+                                        s)
+                   feature-seq (str/upper-case
+                                 (subs
+                                   processed-sequence
+                                   padding
+                                   (- (count processed-sequence) padding)))]
+               {:flanks flanks
+                :feature_seq feature-seq
+                :seq seq-obj
+                :dna-seq feature-seq
+                :sequences [{:sequence (str/replace
+                                         processed-sequence
+                                         (subs processed-sequence
+                                               padding
+                                               (- (count processed-sequence) padding))
+                                         feature-seq)
+                             :comment (str "upper case: feature sequence; lower case: flanking sequences (" strand ")")
+                             :higlight
+                             {:offset padding
+                              :length (count feature-seq)}}]})))
    :description "sequences flanking the feature"})
 
-(defn dna-text [f]
-  {:data nil
+(defn dna-text [f] ; tested with WBsf019129
+  {:data (:feature/dna-text f)
    :description "DNA text of the sequence feature"})
 
 (def widget
