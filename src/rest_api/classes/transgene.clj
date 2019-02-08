16ee7442235a06f16ce3325e40c00d41175f9599
Sibyl <sibyl@wormbase.org>
2019-02-08 19:27:15 +0000

    add human_disease page on transgene page

--- src/rest_api/classes/transgene.clj
+++ src/rest_api/classes/transgene.clj
@@ -3,6 +3,7 @@
     [rest-api.classes.transgene.widgets.phenotypes :as phenotypes]
     [rest-api.classes.transgene.widgets.overview :as overview]
     [rest-api.classes.transgene.widgets.expression :as expression]
+    [rest-api.classes.transgene.widgets.human-diseases :as human-diseases]
     [rest-api.classes.transgene.widgets.isolation :as isolation]
     [rest-api.classes.transgene.widgets.references :as references]
     [rest-api.routing :as routing]))
@@ -13,5 +14,6 @@
    {:overview overview/widget
     :phenotypes phenotypes/widget
     :expr_pattern expression/widget
+    :human_diseases human-diseases/widget
     :isolation isolation/widget
     :references references/widget}})
---
+++ src/rest_api/classes/transgene/widgets/human_diseases.clj
@@ -0,0 +1,23 @@
+(ns rest-api.classes.transgene.widgets.human-diseases
+  (:require
+   [datomic.api :as d]
+   [pseudoace.utils :as pace-utils]
+   [rest-api.classes.do-term.core :as do-term]
+   [rest-api.classes.generic-fields :as generic]
+   [rest-api.formatters.object :as obj :refer [pack-obj]]))
+
+(defn detailed-disease-model [transgene]
+  {:data (let [db (d/entity-db transgene)
+               models (->> (d/q '[:find [?d ...]
+                              :in $ ?e
+                              :where
+                              [?eh :disease-model-annotation.modeled-by-transgene/transgene ?e]
+                              [?d :disease-model-annotation/modeled-by-transgene ?eh]]
+                            db (:db/id transgene))
+                           (map (partial d/entity db)))]
+           (do-term/process-disease-models models))
+   :description "Detailed disease model"})
+
+(def widget
+  {:name generic/name-field
+   :detailed_disease_model detailed-disease-model})
