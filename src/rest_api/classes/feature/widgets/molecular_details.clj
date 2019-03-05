567998f4344d0399411d314e3f687d4585b1df9d
Adam Wright <adam.wright@wormbase.org>
2019-03-05 13:40:46 +0000

    making it so that we are not providing flanking regions if the sequence is not found in the database

--- src/rest_api/classes/feature/widgets/molecular_details.clj
+++ src/rest_api/classes/feature/widgets/molecular_details.clj
@@ -6,6 +6,15 @@
     [rest-api.classes.generic-functions :as generic-functions]
     [rest-api.classes.generic-fields :as generic]))
 
+; This was being displayed instead of the sequence if the sequence was not in the database
+;{:flanks flanks  ; section tested with WBsf000519
+; :seq seq-obj
+; :method method
+; :sequences [{:sequence five-prime-flank
+;              :comment "flanking sequence (upstream)"}
+;             {:sequence three-prime-flank
+;              :comment "flanking sequence (downstream)"}]}
+
 (defn sequence-context [f]
   {:data (when-let [s (:locatable/parent f)]
            (let [five-prime-flank (:feature.flanking-sequences/five-prime (:feature/flanking-sequences f))
@@ -13,15 +22,8 @@
                  flanks [five-prime-flank three-prime-flank]
                  seq-obj (pack-obj s)
                  method (-> f :locatable/method :method/id)]
-             (if (or (some #(= method %) ["SL1" "SL2" "polyA_site" "history feature"])
-                     (not (contains? f :locatable/min)))
-               {:flanks flanks  ; section tested with WBsf000519
-                :seq seq-obj
-                :method method
-                :sequences [{:sequence five-prime-flank
-                             :comment "flanking sequence (upstream)"}
-                            {:sequence three-prime-flank
-                             :comment "flanking sequence (downstream)"}]}
+             (when (not (or (some #(= method %) ["SL1" "SL2" "polyA_site" "history feature"])
+                     (not (contains? f :locatable/min))))
                (let [refseqobj (sequence-fns/genomic-obj f) ;tested with WBsf019129
                      positive-strand-wide (sequence-fns/get-sequence
                                  (conj
