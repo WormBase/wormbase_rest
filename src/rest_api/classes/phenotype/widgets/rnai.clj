6cc9b674047aee6a774fad88d83a8e52bdafb42f
Sibyl <sibyl@wormbase.org>
2020-05-19 16:56:59 +0000

    return counts for rnai not observed phenotype is large WormBase/website#7196

--- src/rest_api/classes/phenotype/widgets/rnai.clj
+++ src/rest_api/classes/phenotype/widgets/rnai.clj
@@ -3,14 +3,8 @@
     [rest-api.formatters.object :as obj :refer  [pack-obj]]
     [rest-api.classes.generic-fields :as generic]))
 
-(defn rnai-info [p obs]
-  (some->> (if obs
-             (map
-               :rnai/_phenotype
-               (:rnai.phenotype/_phenotype p))
-             (map
-               :rnai/_phenotype-not-observed
-               (:rnai.phenotype-not-observed/_phenotype p)))
+(defn rnai-info [p rnais]
+  (some->> (seq rnais)
            (map
              (fn [rnai]
                {:rnai (pack-obj rnai)
@@ -24,12 +18,17 @@
                           (pack-obj strain))}))))
 
 (defn rnai [p]
-  {:data (rnai-info p true)
-   :description (str "The name and WormBase internal ID of " (:db/id p))})
+  (let [rnais (map :rnai/_phenotype (:rnai.phenotype/_phenotype p))]
+    {:data (rnai-info p rnais)
+     :description (str "The name and WormBase internal ID of " (:db/id p))}))
 
 (defn rnai-not [p]
-  {:data (rnai-info p false)
-   :description (str "The name and WormBase internal ID of " (:db/id p))})
+  (let [rnais (map :rnai/_phenotype-not-observed (:rnai.phenotype-not-observed/_phenotype p))
+        count-rnais (count rnais)]
+    {:data (when (< count-rnais 3000)
+             (rnai-info p rnais))
+     :count count-rnais
+     :description (str "The name and WormBase internal ID of " (:db/id p))}))
 
 (def widget
   {:rnai rnai
