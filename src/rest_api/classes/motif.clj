03a171fa5392fbd1cfb050ddf273716bf602a6d6
Adam Wright <adam.wright@wormbase.org>
2020-11-20 20:38:53 +0000

    adding motif overview widget

--- src/rest_api/classes/motif.clj
+++ src/rest_api/classes/motif.clj
@@ -1,13 +1,13 @@
 (ns rest-api.classes.motif
   (:require
     [rest-api.classes.gene.widgets.external-links :as external-links]
-    ;[rest-api.classes.motif.widgets.overview :as overview]
+    [rest-api.classes.motif.widgets.overview :as overview]
     [rest-api.classes.graphview.widget :as graphview]
     [rest-api.routing :as routing]))
 
 (routing/defroutes
   {:entity-ns "motif"
    :widget
-   {;:overview overview/widget
+   {:overview overview/widget
     :graphview graphview/widget
     :external_links external-links/widget}})
