e25908f5cdfbb08747348fefdd4eba13acd6e6ec
Adam Wright <adam.wright@wormbase.org>
2020-11-23 06:46:04 +0000

    releasing expression profile

--- src/rest_api/classes/expr_profile.clj
+++ src/rest_api/classes/expr_profile.clj
@@ -1,6 +1,6 @@
 (ns rest-api.classes.expr-profile
   (:require
-    ;[rest-api.classes.expr-profile.widgets.overview :as overview]
+    [rest-api.classes.expr-profile.widgets.overview :as overview]
     [rest-api.classes.graphview.widget :as graphview]
     [rest-api.classes.expr-profile.widgets.references :as references]
     [rest-api.routing :as routing]))
@@ -8,6 +8,6 @@
 (routing/defroutes
   {:entity-ns "expr-profile"
    :widget
-   {;:overview overview/widget
+   {:overview overview/widget
     :graphview graphview/widget
     :references references/widget}})
--- src/rest_api/classes/expr_profile/widgets/overview.clj
+++ src/rest_api/classes/expr_profile/widgets/overview.clj
@@ -1,12 +1,32 @@
 (ns rest-api.classes.expr-profile.widgets.overview
   (:require
+   [rest-api.db.sequence :as seqdb]
    [rest-api.classes.generic-fields :as generic]
+   [rest-api.classes.generic-functions :as generic-functions]
    [rest-api.formatters.object :as obj :refer [pack-obj]]))
 
-; testing with 0C24D10.5
+; testing with 0C24D10.5, 0W09C5.2 (has sequence)
 
 (defn pcr-data [ep]
-  {:data nil ; need sequence database
+  {:data (if-let [m (first (:expr-profile/expr-map ep))]
+           (let [primer (-> ep :locatable/parent :pcr-product/id)
+                 feature (when-let [species-name (->> ep
+                                                      :locatable/parent
+                                                      :pcr-product/species
+                                                      :species/id)]
+                          (let [g-species (generic-functions/xform-species-name species-name)
+                                sequence-database (seqdb/get-default-sequence-database g-species)
+                                db-spec ((keyword sequence-database) seqdb/sequence-dbs)]
+                           (first (seqdb/get-features db-spec primer))))]
+           {:seq (when feature (str (:seqname feature) ":" (:start feature) ".." (:end feature)))
+            :chromosome (:seqname feature)
+            :radius 4
+            :x_coord (:sk-map/x-coord m)
+            :y_coord (:sk-map/y-coord m)
+            :start (:start feature)
+            :stop (:end feature)
+            :primer primer
+            :mountain (:sk-map/mountain m)}))
    :desciption "pcr data of the expression profile"})
 
 (defn expression-map [ep]
@@ -14,13 +34,12 @@
    :desciption "expression map data for expr_profile"})
 
 (defn pcr-products [ep]
-  {:data nil ; can't find field
-   :keys (keys ep)
-   :dbid (:db/id ep)
+  {:data (when-let [p (:locatable/parent ep)]
+          {(:pcr-product/id ep) (pack-obj p)})
    :desciption "pcr_products for the expression profile"})
 
 (defn profile [ep]
-  {:data nil ;need sequence database
+  {:data nil ;can't find this popuated in the Ace Version
    :desciption "expression profiles for set of genes"})
 
 (defn rnai  [ep]
