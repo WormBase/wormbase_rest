c1c2552ae30acbde1b01b6c4aa3157937fed61c8
Paulo Nuin <nuin@genedrift.org>
2023-03-01 17:57:15 +0000

    starting release WS288, v.1.5.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wormbase/rest-api "1.5.1"
+(defproject wormbase/rest-api "1.5.2"
   :description
   "REST API for retrieving data from datomic on a per widget basis"
   :url "https://github.com/WormBase/wormbase-rest"
