179aa8ae5fd4486d633d61ebd08c96d3776c8849
Paulo Nuin <nuin@genedrift.org>
2024-01-09 17:11:26 +0000

    adding info for WS291

--- scripts/build-appjar.sh
+++ scripts/build-appjar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 artefact="docker/app.jar"
-path=`lein with-profile +datomic-pro,+ddb ring uberjar | \
+path=`~/lein with-profile +datomic-pro,+ddb ring uberjar | \
 	    sed -n 's|^Created \(\/.*standalone.jar\)|\1|p'`
 
 mv "${path}" "${artefact}"
